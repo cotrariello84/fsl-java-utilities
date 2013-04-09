380737b1cbb902c79666113c8a4692b6b112f6af
Noel O'Connor <oconnor.noel@gmail.com>
2013-04-09 13:06:24 +0100

    Updated tests

--- src/main/java/com/fsl/spring/autowiring/byname/County.java
+++ src/main/java/com/fsl/spring/autowiring/byname/County.java
@@ -2,4 +2,14 @@ package com.fsl.spring.autowiring.byname;
 
 public class County {
 
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public County(String name) {
+		this.name=name;
+	}
+	
 }
